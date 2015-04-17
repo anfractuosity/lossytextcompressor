39ca8ee202427e3c44e2a32deed97da17ca3e426
Chris <anfractuosity@anfractuosity.com>
2015-04-17 14:54:07 +0100

    fixed return statement position

--- parse.py
+++ parse.py
@@ -1,23 +1,26 @@
+#!/usr/bin/python3
+
 import textwrap
 from nltk.corpus import wordnet as wn
 import nltk
 import sys
 
+
 def compressWord(word):
-	leng = sys.maxsize
-	sword = ""
-	for i, syn in enumerate(wn.synsets(word, None)):
-		syns = [n.replace('_', ' ') for n in syn.lemma_names]
+	leng = len(word)
+	sword = word
+
+	for i, syn in enumerate(wn.synsets(word)):
+		syns = [n.name().replace('_', ' ') for n in syn.lemmas()]
 
 		if not syns[0] == word:
 			continue
 
 		for s in syns:
-			
 			if len(s) < leng:
 				sword = s
 				leng = len(sword)
-		return sword
+	return sword
 
 def compressFile(filename):
 	out = open(filename).read() 
